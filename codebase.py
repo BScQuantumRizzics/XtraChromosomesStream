7337b48e813f0aee78956b832021758297b21388
Divy-1729 <divy.university@gmail.com>
2024-12-19 18:21:07 -0500

    Create codebase.py

---
+++ codebase.py
@@ -0,0 +1,7 @@
+import random
+insults = open("insults.txt" , "r")
+content = insults.readlines()
+num = len(contents)
+insult_number = rand(0,num)
+if input == "roast":
+  print(content[num].strip()
