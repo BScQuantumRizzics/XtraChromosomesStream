a89386f777451160c10d7a1dfae8275bc608f088
Divy-1729 <divy.university@gmail.com>
2024-12-19 18:48:46 -0500

    Update codebase.py

--- codebase.py
+++ codebase.py
@@ -2,6 +2,7 @@ import random
 insults = open("insults.txt" , "r")
 content = insults.readlines()
 num = len(contents)
-def insulting():
+def insulting() -> str:
+  '''Returns an insult to the user'''
   insult_number = rand(0,num)
   return content[num].strip()
