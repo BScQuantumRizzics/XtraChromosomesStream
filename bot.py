92061559a5ec17e40643e89ef408880b80362b84
Max <81530826+BScQuantumRizzics@users.noreply.github.com>
2024-12-19 16:01:05 -0800

    Update bot.py
    intents moment

--- bot.py
+++ bot.py
@@ -7,7 +7,10 @@ import random
 load_dotenv()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
-bot = commands.Bot(command_prefix="/")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
